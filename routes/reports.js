5b3465e569dfa6442b3156d3fccdb035e9953c2f
tobontob <tobontob@naver.com>
2025-07-01 23:13:05 +0900

    Update reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -4,18 +4,6 @@ const db = require('../config/database');
 const jwt = require('jsonwebtoken');
 const { authenticateToken, requireNotBlocked } = require('./auth');
 
-// 인증 미들웨어 (auth.js에서 복사)
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.status(401).json({ error: '인증 토큰이 필요합니다.' });
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: '유효하지 않은 토큰입니다.' });
-    req.user = user;
-    next();
-  });
-}
-
 // 관리자 권한 체크 미들웨어 (userId=1만 관리자 예시)
 function requireAdmin(req, res, next) {
   if (req.user && req.user.userId === 1) {
